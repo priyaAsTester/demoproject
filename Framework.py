01ed83d97221a4cba0ca2626a1773966cd08e462
priyaAsTester <testerbee742@gmail.com>
2025-01-25 14:06:12 +0530

    my depo framework

---
+++ Framework.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+driver.get("https://demo.guru99.com/")
+driver.find_element(By.XPATH,"//input[@name='uid']").send_keys("mngr604283")
+driver.find_element(By.XPATH,"//input[@type='password']").send_keys("AdUpuvy")
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
+
