8a672d512cae7d8e618a3d8bb6b643be614d7393
priyaAsTester <testerbee742@gmail.com>
2025-01-25 14:30:59 +0530

    added window maximize functionality

--- Framework.py
+++ Framework.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver=webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://demo.guru99.com/")
 driver.find_element(By.XPATH,"//input[@name='uid']").send_keys("mngr604283")
 driver.find_element(By.XPATH,"//input[@type='password']").send_keys("AdUpuvy")
